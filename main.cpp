267c3fbb7eefaa949b6fcaeaaddc5ff5dc6bdbc5
JJ <dontorzz@gmail.com>
2012-09-19 22:37:03 -0700

    main.cpp is added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <gsl/gsl_matrix.h> // if GSL is not installed, you would get error messages
+
+int main(int argc, char *argv) {
+
+  int i, j;
+  gsl_matrix * m = gsl_matrix_alloc (10, 3);
+
+  for (i = 0; i < 10; i++)
+    for (j = 0; j < 3; j++)
+      gsl_matrix_set (m, i, j, 0.23 + 100*i + j);
+
+  for (i = 0; i < 10; i++) 
+    for (j = 0; j < 3; j++)
+      printf ("m(%d,%d) = %g\n", i, j, gsl_matrix_get (m, i, j));
+
+  gsl_matrix_free (m);
+
+  return 0;
+
+}
